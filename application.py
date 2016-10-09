6436758bfb7b6d1730d8d082dd8c93104e18fb89
adiletkadyrov <adiletucti@gmail.com>
2016-10-10 00:07:22 +0800

    Add POST request handling for index route in application.py

--- application.py
+++ application.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask import request, flash
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     """
     display the index page of the portfolio
     """
-    return render_template('index.html')
+    if request.method == 'POST':
+        print(str(request.form['emailInput']))
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
